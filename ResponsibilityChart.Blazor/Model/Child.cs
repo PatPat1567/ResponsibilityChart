809b92c7e52e2968326fcdb8115e7169ecdde496
Patrick Towle <towle_patrick@yahoo.com>
2020-11-18 16:46:33 -0600

    Scaffold tables & modals on manage pages

---
+++ ResponsibilityChart.Blazor/Model/Child.cs
@@ -0,0 +1,8 @@
+namespace ResponsibilityChart.Blazor.Model
+{
+  public class Child
+  {
+    public int Id { get; set; }
+  }
+    
+}
---
+++ ResponsibilityChart.Blazor/Model/Responsibility.cs
@@ -0,0 +1,8 @@
+namespace ResponsibilityChart.Blazor.Model
+{
+  public class Responsibility
+  {
+    public int Id { get; set; }
+  }
+    
+}
